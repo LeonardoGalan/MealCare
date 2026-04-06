9293796c5bc1440f6c7989869b590429126b36bc
LeonardoGalan <leonardogalan2@gmail.com>
2026-04-05 23:06:34 -0400

    fixed prisma generate path which caused issues with backend deployment

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,26 +1,29 @@
-import { PrismaClient } from '../../generated/prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import 'dotenv/config'
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
 
 if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not defined in .env')
+  throw new Error("DATABASE_URL is not defined in .env");
 }
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
-})
+});
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log:
+      process.env.NODE_ENV === "development" ? ["query", "error"] : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  adapter,
-  log: process.env.NODE_ENV === 'development' ? ['query', 'error'] : ['error'],
-})
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
-}
-
-export default prisma
+export default prisma;
